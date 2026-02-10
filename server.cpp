92915f5b45db9113ed275338bb9ed4c4f8344e1f
Manas Trivedi <manas.trivedi.020@gmail.com>
2026-02-10 09:58:07 +0530

    Implement request parsing and response handling for key-value store

--- server.cpp
+++ server.cpp
@@ -14,6 +14,12 @@
 #include <fcntl.h>
 // C++
 #include <vector>
+#include <map>
+#include <string>
+
+// =============
+// Utilities
+// =============
 
 static void msg(const char *msg) {
     fprintf(stderr, "%s\n", msg);
@@ -46,16 +52,18 @@ static void fd_set_nb(int fd) {
 
 const size_t k_max_msg = 32 << 20;
 
-struct Conn {
-    int fd = -1;
-    // intention for event loop
-    bool want_read = false;
-    bool want_write = false;
-    bool want_close = false;
-    // individual buffers for each connection
-    std::vector<uint8_t> incoming;
-    std::vector<uint8_t> outgoing;
+const size_t k_max_args = 3; // for set command; 2 for get and del
+
+// Response status codes
+enum ResponseStatus {
+    RES_OK = 0,
+    RES_ERR = 1,
+    RES_NX = 2,
 };
+
+// =================
+// Buffer Helpers
+// =================
 
 static void buf_append(std::vector<uint8_t> &buf, const uint8_t *data, size_t len) {
     buf.insert(buf.end(), data, data + len);
@@ -64,6 +72,113 @@ static void buf_append(std::vector<uint8_t> &buf, const uint8_t *data, size_t le
 static void buf_consume(std::vector<uint8_t> &buf, size_t n) {
     buf.erase(buf.begin(), buf.begin() + n);
 }
+
+// ===========
+// Protocol
+// ===========
+
+static bool read_u32(const uint8_t *&cur, const uint8_t *end, uint32_t &out) {
+    if(cur + 4 > end) {
+        return false;
+    }
+
+    memcpy(&out, cur, 4);
+    cur += 4;
+
+    return true;
+}
+
+static bool read_str(const uint8_t *&cur, const uint8_t *end, uint32_t n, std::string &out) {
+    if(cur + n > end) {
+        return false;
+    }
+
+    out.assign(cur, cur + n);
+    cur += n;
+    return true;
+}
+
+static int32_t parse_request(const uint8_t *data, size_t size, std::vector<std::string> &out){
+    const uint8_t *end = data + size;
+
+    uint32_t nstr = 0;
+
+    if(!read_u32(data, end, nstr)) {
+        return -1;
+    }
+
+    if(nstr > k_max_args) {
+        return -1;
+    }
+
+    while(out.size() < nstr) {
+        uint32_t len = 0;
+
+        if(!read_u32(data, end, len)) {
+            return -1;
+        }
+
+        out.push_back(std::string());
+
+        if(!read_str(data, end, len, out.back())) {
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+struct Response {
+    uint32_t status = 0;
+    std::vector<uint8_t> data;
+};
+
+static void make_response(const Response &resp, std::vector<uint8_t> &out) {
+    uint32_t resp_len = 4 + (uint32_t)resp.data.size();
+    buf_append(out, (const uint8_t *)&resp_len, 4);
+    buf_append(out, (const uint8_t *)&resp.status, 4);
+    buf_append(out, resp.data.data(), resp.data.size());
+}
+
+// =====================
+// Application Logic
+// =====================
+
+// trivial rn will implement custom in next iteration
+static std::map<std::string, std::string> g_data;
+
+static void do_request(std::vector<std::string> cmd, Response &out) {
+    if(cmd.size() == 2 && cmd[0] == "get") {
+        auto it = g_data.find(cmd[1]);
+        if(it == g_data.end()) {
+            out.status = RES_NX;
+            return;
+        }
+        const std::string &val = it->second;
+        out.data.assign(val.begin(), val.end());
+    } else if(cmd.size() == 3 && cmd[0] == "set") {
+        g_data[cmd[1]].swap(cmd[2]);
+    } else if(cmd.size() == 2 && cmd[0] == "del") {
+        g_data.erase(cmd[1]);
+    } else {
+        out.status = RES_ERR;
+    }
+}
+
+// ======================
+// Connection Handling
+// ======================
+
+struct Conn {
+int fd = -1;
+    // intention for event loop
+    bool want_read = false;
+    bool want_write = false;
+    bool want_close = false;
+    // individual buffers for each connection
+    std::vector<uint8_t> incoming;
+    std::vector<uint8_t> outgoing;
+};
 
 static Conn* handle_accept(int fd) {
     struct sockaddr_in client_addr = {};
@@ -89,7 +204,6 @@ static Conn* handle_accept(int fd) {
     return conn;
 }
 
-// we try to see if the data in the incoming buffer is enough for one full msg
 static bool try_one_request(Conn* conn) {
     if(conn->incoming.size() < 4) {
         return false; // don't even have enough for a header yet
@@ -110,15 +224,18 @@ static bool try_one_request(Conn* conn) {
 
     const uint8_t *request = &conn->incoming[4];
 
-    // application logic rn just print the msg
-    printf("Client says: len: %d data:%.*s\n", len, len < 100 ? len : 100, request);
+    // application logic : K-V Store (simple without data structures)
 
-    // response (echo back)
-    buf_append(conn->outgoing, (const uint8_t *)& len, 4);
-    buf_append(conn->outgoing, request, len);
+    std::vector<std::string> cmd;
 
-    // empty parsed buffer
-    buf_consume(conn->incoming, 4 + len); // not clearing bcz we might have some bytes of new msg
+    if(parse_request(request, len, cmd) < 0) {
+        conn->want_close = true;
+        return false;
+    }
+
+    Response resp;
+    do_request(cmd, resp);
+    make_response(resp, conn->outgoing);
 
     return true;
 }
